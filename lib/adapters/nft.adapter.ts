ed89863a65de97d20186028d7193e3db4b900a7d
itsDavidev <davidlez2mgk@gmail.com>
2023-01-16 14:34:22 -0600

    🔧 config adapter for return image thumbnail

--- lib/adapters/nft.adapter.ts
+++ lib/adapters/nft.adapter.ts
@@ -1,15 +1,23 @@
-import { type OwnedNft, type Nft } from 'npm:alchemy-sdk';
+import { type Nft, type OwnedNft } from 'npm:alchemy-sdk';
 
 import { type IAdapterNFT } from '~lib/interfaces/AdapterNFT.interface.ts';
 import { resolvePathIpfs } from '~lib/utils/resolvePathIpfs.util.ts';
 
+export const POSITION_NFT_INITIAL = 0;
+
 export function adapterNFTs(nft: OwnedNft | Nft): IAdapterNFT {
+  const image: string | undefined = resolvePathIpfs(nft.rawMetadata?.image);
+  const thumbnail: string | undefined = resolvePathIpfs(
+    nft.media[POSITION_NFT_INITIAL].thumbnail
+  );
+
   return {
-    name: nft.title || 'NFTDeCoDeD',
-    image: resolvePathIpfs(nft.rawMetadata?.image),
+    name: nft.title || 'DavNFT - INCOGNITO',
     contract: nft.contract.address,
     id: nft.tokenId,
     uri: nft.tokenUri?.gateway,
     price: nft.contract.openSea?.floorPrice,
+    image,
+    thumbnail,
   };
 }
--- lib/interfaces/AdapterNFT.interface.ts
+++ lib/interfaces/AdapterNFT.interface.ts
@@ -1,8 +1,16 @@
 export interface IAdapterNFT {
+  /** Name : name of the NFT */
   name: string;
+  /** Image : RAW image of the NFT */
   image: string | undefined;
+  /** Contract : address of the contract */
   contract: string;
+  /** Id : id of the NFT */
   id: string;
+  /** Uri : uri of collection NFTS*/
   uri?: string | undefined;
+  /** Price : price of the NFT */
   price?: number | undefined;
+  /** THUMBNAIL : MINIMAL image */
+  thumbnail?: string | undefined;
 }
